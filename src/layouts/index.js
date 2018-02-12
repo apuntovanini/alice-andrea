ffd1e41b11e3b98c91224f7951e723204e41cbed
Andrea Vanini <andrea.vanini@uidu.org>
2018-02-12 19:42:25 +0100

    Change name

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,35 +1,35 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import Helmet from 'react-helmet'
+import React from "react";
+import PropTypes from "prop-types";
+import Helmet from "react-helmet";
 
-import Header from '../components/Header'
-import './index.css'
+import Header from "../components/Header";
+import "./index.css";
 
 const TemplateWrapper = ({ children }) => (
   <div>
     <Helmet
-      title="Gatsby Default Starter"
+      title="Alice & Andrea \\ 12_05_2018"
       meta={[
-        { name: 'description', content: 'Sample' },
-        { name: 'keywords', content: 'sample, something' },
+        { name: "description", content: "Sample" },
+        { name: "keywords", content: "sample, something" }
       ]}
     />
     <Header />
     <div
       style={{
-        margin: '0 auto',
+        margin: "0 auto",
         maxWidth: 960,
-        padding: '0px 1.0875rem 1.45rem',
-        paddingTop: 0,
+        padding: "0px 1.0875rem 1.45rem",
+        paddingTop: 0
       }}
     >
       {children()}
     </div>
   </div>
-)
+);
 
 TemplateWrapper.propTypes = {
-  children: PropTypes.func,
-}
+  children: PropTypes.func
+};
 
-export default TemplateWrapper
+export default TemplateWrapper;

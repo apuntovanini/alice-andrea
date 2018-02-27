e131280c2d6ade42200740409a4426b36b93175e
Andrea Vanini <andrea.vanini@uidu.org>
2018-02-27 23:07:31 +0100

    Fix last deploy error

--- src/layouts/index.jsx
+++ src/layouts/index.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Helmet from 'react-helmet';
 import { Element } from 'react-scroll';
-import 'bootstrap';
+// import 'bootstrap';
 
 import {
   Header,
